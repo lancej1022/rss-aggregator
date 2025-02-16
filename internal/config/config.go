6d735e6fe15ab53fb0bc9ba3ea1ef8da320bcb9d
lancej1022 <46359098+lancej1022@users.noreply.github.com>
2025-02-16 13:38:15 -0700

    Tweaks

--- internal/config/config.go
+++ internal/config/config.go
@@ -3,7 +3,10 @@ package config
 import (
 	"encoding/json"
 	"os"
+	"path/filepath"
 )
+
+const configFileName = ".gatorconfig.json"
 
 type Config struct {
 	DbUrl           string `json:"db_url"`
@@ -11,26 +14,57 @@ type Config struct {
 }
 
 // TODO: should this be a value receiver instead of a pointer receiver?
-func (c *Config) SetUser(name string) {
+func (c *Config) SetUser(name string) error {
 	c.CurrentUserName = name
+	return write(*c)
+}
 
-	// homeDir, err := os.UserHomeDir()
-	// if err != nil {
-	// 	panic(err)
-	// }
-	// configFilePath := filepath.Join(homeDir, configFileName)
+func getConfigFilePath() (string, error) {
+	homeDir, err := os.UserHomeDir()
+	if err != nil {
+		return "", err
+	}
+
+	configPath := filepath.Join(homeDir, configFileName)
+	return configPath, nil
+}
+
+func Read() (Config, error) {
+	configPath, err := getConfigFilePath()
+	if err != nil {
+		return Config{}, err
+	}
+
+	file, err := os.Open(configPath)
+	if err != nil {
+		return Config{}, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := Config{}
+	if err := decoder.Decode(&cfg); err != nil {
+		// TODO: the error occurs here...
+		return Config{}, err
+	}
+
+	return cfg, nil
+}
+
+func write(c Config) error {
 	configPath, err := getConfigFilePath()
 	if err != nil {
 		panic(err)
 	}
 	file, err := os.Create(configPath)
 	if err != nil {
-		panic(err)
+		return err
 	}
 	defer file.Close()
 
 	encoder := json.NewEncoder(file)
 	if err := encoder.Encode(c); err != nil {
-		panic(err)
+		return err
 	}
+	return nil
 }
